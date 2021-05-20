baaf3ba25ce99c4b8a7eb6c14029483632681bff
Cesar Sportore <csportore@192.168.15.80>
2021-05-19 22:39:26 -0300

    Voltando aos estudos!

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,7 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients:Ingredient[] = [
+  ingredients : Ingredient[] = [
     new Ingredient('Apple', 5),
     new Ingredient('Tomato', 10),
   ];
